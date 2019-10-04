c1cafb32181f166fa6e322ad9d3a5056b928173e
Jan Doležel <dolezel.jan@gmail.com>
2019-10-04 15:13:17 +0200

    Refactoring code, removing deprecated componentWillReceiveProps and computing csv when needed (#121)

--- src/lib/csv.js
+++ src/lib/csv.js
@@ -1,53 +1,36 @@
 const newLine = '\r\n';
 
 export default function csv(columns, datas, separator = ',', noHeader = false) {
-  let columnOrder;
+  const columnOrder = columns
+    ? columns.map((v) => (typeof v === 'string' ? v : v.id))
+    : datas.reduce(
+      (acc, v) => (Array.isArray(v) ? acc : acc.concat(Object.keys(v))),
+      [],
+    ).filter((value, index, self) => self.indexOf(value) === index);
+
   const content = [];
-  const column = [];
 
-  if (columns) {
-    columnOrder = columns.map((v) => {
-      if (typeof v === 'string') {
-        return v;
-      }
-      if (!noHeader) {
-        column.push((typeof v.displayName !== 'undefined') ? v.displayName : v.id);
-      }
-      return v.id;
-    });
-    if (column.length > 0) {
-      content.push(column.join(separator));
-    }
-  } else {
-    columnOrder = [];
-    datas.forEach((v) => {
-      if (!Array.isArray(v)) {
-        columnOrder = columnOrder.concat(Object.keys(v));
-      }
-    });
-    if (columnOrder.length > 0) {
-      columnOrder = columnOrder.filter((value, index, self) => self.indexOf(value) === index);
-
-      if (!noHeader) {
-        content.push(columnOrder.join(separator));
-      }
-    }
+  if (!noHeader && columnOrder.length > 0) {
+    const headers = columns
+      ? columns.map((v) => {
+        if (typeof v === 'string') {
+          return v;
+        } if (typeof v.displayName !== 'undefined') {
+          return v.displayName;
+        }
+        return v.id;
+      })
+      : columnOrder;
+    content.push(headers.join(separator));
   }
 
   if (Array.isArray(datas)) {
-    datas.map((v) => {
-      if (Array.isArray(v)) {
-        return v;
-      }
-      return columnOrder.map((k) => {
-        if (typeof v[k] !== 'undefined') {
-          return v[k];
-        }
-        return '';
-      });
-    }).forEach((v) => {
+    datas.map((v) => (Array.isArray(v)
+      ? v
+      : columnOrder.map((k) => (typeof v[k] === 'undefined' ? '' : v[k])))).forEach((v) => {
       content.push(v.join(separator));
     });
   }
+
   return content.join(newLine);
 }
